598583f3d6d60b4cbc60829425a02a0e44d41821
PhucVo <fakermsi1999@gmail.com>
2019-11-02 22:23:55 +0700

    use case 2: add new contact, validation form and submit form

--- src/main/java/vn/hcmus/fit/mycontact/entity/Contact.java
+++ src/main/java/vn/hcmus/fit/mycontact/entity/Contact.java
@@ -3,6 +3,8 @@ package vn.hcmus.fit.mycontact.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @Entity
@@ -12,9 +14,12 @@ public class Contact {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+//    validator constraint
+    @NotEmpty // Không được bỏ trống tên liên hệ
     @Column(nullable = false)
     private String name;
 
+    @Email // email phải đúng định dạng
     private String email;
 
     private String phone;
